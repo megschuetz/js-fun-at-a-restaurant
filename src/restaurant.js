8c96e08ee831c1627a347bebd1219acf59476942
Megan Schuetz <mschuetz411@gmail.com>
2022-03-27 19:39:06 -0600

    addRestaurant

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,30 @@
+ function createRestaurant(pizza) {
+//
+  return {
+   name: pizza,
+   menus: {
+     breakfast:[],
+     lunch: [],
+     dinner: []
+   },
+   };
+
+ };
+
+function addMenuItem(rest, item){
+  if (rest.menus[item.type].includes(item)) {
+  return rest.menus[item.type]
+} else {
+  rest.menus[item.type].push(item)
+}
+};
+
+
+
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  //removeMenuItem
 }
